bb8fe92e531b7f1a870391167d25cc7df03838dc
GalloSimone <gallosimonecz@gmail.com>
2025-03-18 15:50:19 +0100

    milestone 2 completata

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,25 @@ import './App.css'
 function App() {
   const [input, setInput] = useState("")
   const [result, setResult] = useState([])
+  const [debounceTimeout,setDebaunceTimeout]=useState(null)
+
 useEffect(()=>{
   if(input.trim()===""){
     setResult([]);
     return;
   }
+  if (debounceTimeout) {
+    clearTimeout(debounceTimeout);
+  }
+const timeoutId=setTimeout(()=>{
   fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/products?search=${input}`)
-  .then((res)=>res.json())
-  .then((data)=>setResult(data))
-  .catch((error)=>console.error("errore"))
-},[input]);
+    .then((res)=>res.json())
+    .then((data)=>setResult(data))
+     .catch((error)=>console.error("errore"))
+},300)
+setDebaunceTimeout(timeoutId) 
+return()=>clearTimeout(timeoutId)
+ },[input]);
   return (
     <>
      <h1>hello world</h1>
@@ -23,7 +32,20 @@ useEffect(()=>{
      value={input}
      onChange={(e)=>setInput(e.target.value)}
      />
-     
+     <div>
+      {result.length>0 &&(
+        <ul>
+        {result.map((item, index)=>(
+        <li key={index}>
+          <img src={item.image} alt={item.name} />
+          <p>nome:{item.name}</p>
+          <p>descrizione:{item.description}</p>
+          <p>prezzo:{item.price}</p>
+          </li>
+        ))}
+          </ul>
+      )}
+     </div>
     </>
   )
 }
